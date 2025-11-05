84973f4914b5dd380cc860f79164ad2d877f9432
DanielaDominguezCruz <211u0371@alumno.itssat.edu.mx>
2025-11-05 14:39:24 -0600

    Create MainSimulador.java

---
+++ src/main/java/proyectocriptografia/consola/MainSimulador.java
@@ -0,0 +1,8 @@
+package proyectocriptografia.consola;
+
+/** Punto de entrada del simulador. */
+public class MainSimulador {
+    public static void main(String[] args) {
+        new MenuConsola().mostrar();
+    }
+}
